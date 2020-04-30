a99a7fe17d260211ac60431164b5ba4250184dd1
Luis Colorado <luiscoloradourcola@gmail.com>
2020-04-30 16:15:02 +0300

    Add header to mkstubs.sh and update copyright note in gen. code
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- bootstrap/mkstubs.sh
+++ bootstrap/mkstubs.sh
@@ -1,3 +1,13 @@
+# mkstubs.sh -- shell script to build a library of stubs to be
+# called for non implemented routines on rule reduction.
+# All these routines are included in a library that is linked at
+# the end, to cover all the routines not implemented yet, and
+# making the testing possible.
+# Author: Luis Colorado <luiscoloradourcola@gmail.com>
+# Date: Thu Apr 30 16:10:55 EEST 2020
+# Copyright: (c) 2018-2020 LUIS COLORADO.  All rights reserved.
+# License: BSD.
+
 LIB=libstubs.a
 
 for N
@@ -6,7 +16,7 @@ do
 /* stub_${N}.c --- stub callback to allow to compile unimplemented stuff.
  * Author: Luis Colorado <luiscoloradourcola@gmail.com>
  * Date: $(LANG=C date)
- * Copyright: (C) LUIS COLORADO.  All rights reserved.
+ * Copyright: (C) 2018-$(date +%Y) LUIS COLORADO.  All rights reserved.
  * License: BSD
  * NOTE: This file generated automatically.  DON\'T EDIT.
  */
@@ -18,9 +28,13 @@ do
 
 int reduce_${N}_cb(union tree_node nod)
 {
+	static int already_issued = 0;
+	if (already_issued)
+		return 0;
 	if (global.flags & GL_FLAG_SHOW_STUBS) {
 		WARN("NOT IMPLEMENTED YET. USING "
 			"${LIB}(reduce_${N}_cb()) FUNCTION\\n");
+		already_issued = 1;
 	}
 	return 0;
 }
